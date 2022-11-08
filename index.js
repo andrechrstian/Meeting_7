cbc14bdda09bf649938b035e61d0b3f818d9b47a
andrechrstian <andrechristianacl@gmail.com>
2022-11-08 23:38:50 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+//soal pertama
+
+let totalAge = 80;
+let pAyah = 4;
+let pAnak = 1;
+
+let total = pAyah + pAnak;
+let umurAnak = totalAge / total;
+
+console.log(umurAnak);
+
+//x + y = 80
+//y = 4x
+//x + 4x = 80
+// x = 80/5
+
+
+//soal kedua case 3
+
+const a = new Date ();
+
+const FullYear = a.getFullYear ();
+const Month = a.getMonth ();
+const Date1 = a.getDate ();
+
+console.log (FullYear, Month, Date1);
+console.log (FullYear, Month, Date1-1);
+console.log (FullYear, Month, Date1+1);
+
+//Soal case 5
+
+const b = "Andre Christian"
+console.log (b.length);
